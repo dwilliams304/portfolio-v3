d6fb5eee9a7a83a670baeb48aa283ae0816a9521
dwilliams304 <d.williams0434@gmail.com>
2024-12-04 20:15:30 -0500

    wip: sections list

--- src/components/layout/SectionsList.tsx
+++ src/components/layout/SectionsList.tsx
@@ -1,13 +1,11 @@
 export default function SectionsList(){
     return(
         <div className="sl-container">
-            <h2 className="sl-header">Header</h2>
+            <h2 className="sl-header">Sections</h2>
             <ul className="sl-list">
-                <li>Section 1</li>
-                <li>Section 2</li>
-                <li>Section 3</li>
-                <li>Section 4</li>
-                <li>Section 5</li>
+                <li className="txt-underline-effect">Home</li>
+                <li className="txt-underline-effect">About</li>
+                <li className="txt-underline-effect">Projects</li>
             </ul>
         </div>
     )
