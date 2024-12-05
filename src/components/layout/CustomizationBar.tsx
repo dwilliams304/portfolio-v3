89e21f7643cdd0719803e29f72e22df26bdbb882
dwilliams304 <d.williams0434@gmail.com>
2024-12-05 17:09:21 -0500

    wip: customization bar

--- src/components/layout/CustomizationBar.tsx
+++ src/components/layout/CustomizationBar.tsx
@@ -1,26 +1,76 @@
+import { useState } from "react";
+
+import { 
+    Brevities, 
+    Experiences, 
+    Styles 
+} from "../../data/types";
+
 import "./_layouts.css";
 
+
 export default function CustomizationBar(){
+    const [experience, setExperience] = useState(Experiences[0]);
+    const [styles, setStyles] = useState(Styles[experience]);
+    
+    const onChange = (e: any) => {
+        const {name, value} = e.target;
+        if(name === "experience"){
+            setStyles(Styles[value as keyof typeof Styles]);
+            setExperience(value);
+        }
+    }
+
     return(
         <div className="cb-hover-area">
             <div className="cb-container">
-                <select>
-                    <option>Experience 1</option>
-                    <option>Experience 2</option>
-                    <option>Experience 3</option>
-                    <option>Experience 4</option>
-                    <option>Experience 5</option>
-                </select>
-                <select>
-                    <option>Brevity 1</option>
-                    <option>Brevity 2</option>
-                    <option>Brevity 3</option>
-                    <option>Brevity 4</option>
-                    <option>Brevity 5</option>
-                </select>
-                <button>
-                    Light Mode/Dark Mode
-                </button>
+                <div>
+                    <label htmlFor="experience">Experience</label>
+                    <select name="experience"
+                    onChange={onChange}>
+                        {
+                            Experiences.map(experience => (
+                                <option key={experience}
+                                value={experience}>
+                                    {experience}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="style">Style</label>
+                    <select name="style"
+                    onChange={onChange}>
+                        {
+                            styles.map(style => (
+                                <option key={style}>
+                                    {style}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="brevity">Brevity</label>
+                    <select name="brevity"
+                    onChange={onChange}>
+                        {
+                            Brevities.map(brevity => (
+                                <option key={brevity}
+                                value={brevity}>
+                                    {brevity}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                {/* <button>
+                    
+                </button> */}
             </div>
         </div>
     )
--- src/data/types.ts
+++ src/data/types.ts
@@ -6,3 +6,29 @@ export type Project = {
     projectDescription: string;
     links: string[];
 }
+
+
+const Brevities = ["Qck", "Brief", "Normal", "More Text", "Super Long Winded"] as const;
+const Experiences = ["Default", "Game"] as const;
+
+const Styles = {
+    Default: [
+        "Space",
+        "Retro",
+        "Shopping",
+        "Music Platform",
+        "Modern",
+        "Minimal",
+        "Grid"
+    ],
+    Game: [
+        "Mystery",
+        "Shooter"
+    ]
+}
+
+export {
+    Brevities,
+    Experiences,
+    Styles
+}
