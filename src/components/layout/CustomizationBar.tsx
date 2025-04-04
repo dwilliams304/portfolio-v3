5a337096a1894255d0e0bae49066e7bac31102f5
dwilliams304 <d.williams0434@gmail.com>
2025-04-04 14:45:43 -0400

    customization bar can be opened/closed

--- src/components/layout/CustomizationBar.tsx
+++ src/components/layout/CustomizationBar.tsx
@@ -1,77 +1,85 @@
 import { useState } from "react";
 
-import { 
-    Brevities, 
-    Experiences, 
-    Styles 
+import {
+    Brevities,
+    Experiences,
+    Styles
 } from "../../data/types";
 
 import "./_layouts.css";
 
 
-export default function CustomizationBar(){
+export default function CustomizationBar() {
     const [experience, setExperience] = useState(Experiences[0]);
     const [styles, setStyles] = useState(Styles[experience]);
 
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
     const onChange = (e: any) => {
-        const {name, value} = e.target;
-        if(name === "experience"){
+        const { name, value } = e.target;
+        if (name === "experience") {
             setStyles(Styles[value as keyof typeof Styles]);
             setExperience(value);
         }
     }
 
-    return(
-        <div className="cb-hover-area">
-            <div className="cb-container">
-                <div>
-                    <label htmlFor="experience">Experience</label>
-                    <select name="experience"
-                    onChange={onChange}>
-                        {
-                            Experiences.map(experience => (
-                                <option key={experience}
-                                value={experience}>
-                                    {experience}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
+    return (
+        <>
+            <button className={`cb-toggle-button ${menuIsOpen ? "open" : ""}`} onClick={() => setMenuIsOpen(prev => !prev)}>
+                <span>&#9660;</span>
+            </button>
 
-                <div>
-                    <label htmlFor="style">Style</label>
-                    <select name="style"
-                    onChange={onChange}>
-                        {
-                            styles.map(style => (
-                                <option key={style}>
-                                    {style}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
+            <div className={`cb-hover-area ${menuIsOpen ? 'open' : ''}`}>
+                <div className="cb-container">
+                    <div>
+                        <label htmlFor="experience">Experience</label>
+                        <select name="experience"
+                            onChange={onChange}>
+                            {
+                                Experiences.map(experience => (
+                                    <option key={experience}
+                                        value={experience}>
+                                        {experience}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
 
-                <div>
-                    <label htmlFor="brevity">Brevity</label>
-                    <select name="brevity"
-                    onChange={onChange}>
-                        {
-                            Brevities.map(brevity => (
-                                <option key={brevity}
-                                value={brevity}>
-                                    {brevity}
-                                </option>
-                            ))
-                        }
-                    </select>
-                </div>
+                    <div>
+                        <label htmlFor="style">Style</label>
+                        <select name="style"
+                            onChange={onChange}>
+                            {
+                                styles.map(style => (
+                                    <option key={style}>
+                                        {style}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
 
-                {/* <button>
+                    <div>
+                        <label htmlFor="brevity">Brevity</label>
+                        <select name="brevity"
+                            onChange={onChange}>
+                            {
+                                Brevities.map(brevity => (
+                                    <option key={brevity}
+                                        value={brevity}>
+                                        {brevity}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+
+                    {/* <button>
                     
                 </button> */}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -43,7 +43,7 @@ export function App(){
 
   return(
     <div className="site-container">
-      {/* <CustomizationBar /> */}
+      <CustomizationBar />
       <SectionsList />
       <Header />
       <AboutCard />
